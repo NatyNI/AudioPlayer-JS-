966d198a5e71136d11e0a89e1561ea1035b4b62a
NatyNi <natynicolae2002@gmail.com>
2024-12-02 14:30:00 +0200

    Add newProject

---
+++ newProject/new.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const audio = new Audio("https://picuridinfaguri.s3.eu-west-2.amazonaws.com/cezar_sandra-cugetul-2024-10-24.mp3")
+
+const playPauseBtn = document.getElementById("play-pause")
+const currentTimE = document.getElementById("current-time")
+const timeRemaining = document.getElementById("time-remaining")
+const progressBar = document.getElementById("progress-bar")
+const volumeControl = document.getElementById("volume-control")
+const speedButton = document.getElementById("button-speed")
+
+volumeControl.addEventListener("wheel", (event) =>{
+    event.preventDefault
+    const deltaY = event.deltaY  
+    console.log(audio.volume)
+    if (deltaY < 0) {
+        audio.volume = Math.max(audio.volume - 0.005, 0)   
+    } 
+    else if (deltaY > 0) {
+        audio.volume = Math.max(audio.volume + 0.005, 0.09)
+    }
+} )
+
+playPauseBtn.addEventListener("click", ()=> {
+    console.log(audio.volume)
+    audio.volume = volumeControl.value / 100;
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.textContent='Pause';
+    } else {
+        audio.pause();
+        playPauseBtn.textContent='Play';
+    }
+})
+
+audio.addEventListener('timeupdate', function() {
+    
+    currentTimE.textContent = formatTime(audio.currentTime);
+    timeRemaining.textContent =  formatTime(audio.duration - audio.currentTime);
+
+});
+
+progressBar.addEventListener("input", () => {
+    audio.currentTime = (progressBar.value / 100) * audio.duration;
+    currentTimE.textContent = formatTime(audio.currentTime);
+})
+
+volumeControl.addEventListener("input", ()=>{
+    audio.volume = volumeControl.value / 100;
+
+});
+
+speedButton.addEventListener("click", () => {
+    if (audio.playbackRate == 1){
+        audio.playbackRate = 1.2;
+        speedButton.textContent = `${audio.playbackRate}X`;
+    }
+    else if (audio.playbackRate == 1.2) {
+        audio.playbackRate = 1.5;
+        speedButton.textContent = `${audio.playbackRate}X`;
+    }
+    else if (audio.playbackRate == 1.5) {
+        audio.playbackRate = 2;
+        speedButton.textContent = `${audio.playbackRate}X`;
+    }
+    else if (audio.playbackRate == 2) {
+        audio.playbackRate = 1;
+        speedButton.textContent = `${audio.playbackRate}X`;
+    }
+})
+
+function formatTime(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
+
+
+
+
